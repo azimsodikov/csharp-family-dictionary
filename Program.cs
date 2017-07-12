b0258f03e065b0f9729f137e87f8d6c444cb0e6b
Azim <azimsadikov@gmail.com>
2017-07-12 15:16:03 -0500

    "dictionary is done baby"

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace family_dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+
+        myFamily.Add("qoshnim", new Dictionary<string, string>(){ 
+            {"name", "Hoshim"},
+            {"age", "40"}
+        });
+        myFamily.Add("sister", new Dictionary<string, string>(){ 
+            {"name", "Koshim"},
+            {"age", "29"}
+        });
+        myFamily.Add("friend", new Dictionary<string, string>(){ 
+            {"name", "Chomich"},
+            {"age", "42"}
+        });
+        myFamily.Add("buddy", new Dictionary<string, string>(){ 
+            {"name", "Chelak"},
+            {"age", "49"}
+        });
+        myFamily.Add("dude", new Dictionary<string, string>(){ 
+            {"name", "Paqir"},
+            {"age", "44"}
+        });
+        myFamily.Add("jerom", new Dictionary<string, string>(){ 
+            {"name", "Talinka"},
+            {"age", "32"}
+        });
+        // Next, iterate over eac
+
+            foreach (var item in myFamily)
+            {
+                Console.WriteLine($"{item.Value["name"]} is my {item.Key} and is {item.Value["age"]} years old");
+            }
+        }
+    }
+}
